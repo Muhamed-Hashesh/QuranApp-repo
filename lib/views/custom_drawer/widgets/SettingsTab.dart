1f4418a39428d9281f127187f0638ced0d2a6fcb
MohamedElsay0d <me827794@gmail.com>
2024-09-03 21:30:05 -0700

    add dark theme button in drawer

--- lib/views/custom_drawer/widgets/SettingsTab.dart
+++ lib/views/custom_drawer/widgets/SettingsTab.dart
@@ -1,35 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
-class SettingsTab extends StatelessWidget {
+class SettingsTab extends StatefulWidget {
   @override
+  State<SettingsTab> createState() => _SettingsTabState();
+}
+
+class _SettingsTabState extends State<SettingsTab> {
+  @override
+  bool isNightModeEnabled = false;
+
   Widget build(BuildContext context) {
     return ListView(
       padding: const EdgeInsets.all(8.0),
       children: [
         ListTile(
           title: const Text('التفسير'),
-          leading: const Icon(Icons.book),
+          leading: const Icon(Iconsax.book),
           onTap: () {},
         ),
         ListTile(
           title: const Text('الأجزاء'),
-          leading: const Icon(Icons.book),
+          leading: const Icon(Iconsax.book1),
           onTap: () {},
         ),
         ListTile(
           title: const Text('الأحزاب'),
-          leading: const Icon(Icons.book),
+          leading: const Icon(Iconsax.book4),
           onTap: () {},
         ),
         ListTile(
-          title: const Text('المسجدات'),
-          leading: const Icon(Icons.book),
+          title: const Text('السجدات'),
+          leading: const Icon(Icons.book_outlined),
           onTap: () {},
         ),
         ListTile(
           title: const Text('الركوع'),
           leading: const Icon(Icons.book),
           onTap: () {},
+        ),
+        ListTile(
+          title: const Text(
+            'تفعيل الوضع الليلي',
+            maxLines: 1,
+            softWrap: false,
+          ),
+          trailing: Switch(
+            value: isNightModeEnabled,
+            onChanged: (value) {
+              setState(() {
+                isNightModeEnabled = value;
+              });
+            },
+            activeColor: Theme.of(context).colorScheme.primary,
+          ),
+          leading: const Icon(
+            Icons.nightlight_round,
+          ),
         ),
         ListTile(
           title: const Text('الصفحة الرئيسية'),
