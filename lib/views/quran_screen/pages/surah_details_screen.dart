2c09fe11c6fae8c6eb7749ebcea862004cf50b87
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-09-10 03:07:03 +0300

    quran page format

--- lib/views/quran_screen/pages/surah_details_screen.dart
+++ lib/views/quran_screen/pages/surah_details_screen.dart
@@ -8,6 +8,7 @@ import 'package:quran_project/views/custom_drawer/pages/custom_drawer.dart';
 import 'package:quran_project/views/quran_screen/cubit/change_scene_cubit.dart';
 import 'package:quran_project/views/quran_screen/helpers/app_bar_customization.dart';
 import 'package:quran_project/views/quran_screen/pages/quran_page.dart';
+import 'package:quran_project/views/quran_screen/pages/quran_surah_page_builder.dart';
 import 'package:quran_project/widgets/custom_appbar.dart';
 
 class SurahDetailScreen extends StatefulWidget {
@@ -67,112 +68,6 @@ class _SurahDetailScreenState extends State<SurahDetailScreen> {
               }
               return const Center(child: CircularProgressIndicator());
             },
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class QuranSurahPageBuilder extends StatelessWidget {
-  const QuranSurahPageBuilder({
-    super.key,
-    required this.pages,
-    required this.versesByPage,
-    required this.surahNumber,
-  });
-
-  final List<int> pages;
-  final Map<int, List<int>> versesByPage;
-  final int surahNumber;
-
-  @override
-  Widget build(BuildContext context) {
-    return PageView.builder(
-      physics: const ClampingScrollPhysics(),
-      itemCount: pages.length,
-      itemBuilder: (context, index) {
-        final pageNumber = pages[index];
-        final verses = versesByPage[pageNumber] ?? [];
-        final isFirstPage = index == 0;
-
-        return SingleChildScrollView(
-          physics: const ClampingScrollPhysics(),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Column(
-              children: [
-                24.height,
-                if (isFirstPage)
-                  Column(
-                    children: [
-                      ChapterHeader(surahNumber: surahNumber),
-                      24.height,
-                      surahNumber != 9 && surahNumber != 1
-                          ? const Image(
-                              image: AssetImage('assets/images/basmala.png'),
-                              width: 250,
-                            )
-                          : const SizedBox(),
-                    ],
-                  ),
-                // if (surahNumber != 9) ChapterHeader(surahNumber: surahNumber),
-                RichText(
-                  textAlign: TextAlign.justify,
-                  text: TextSpan(
-                    style: TextStyle(
-                      fontFamily: 'UthmanicHafs',
-                      fontSize: 23,
-                      color: Theme.of(context).colorScheme.primary,
-                    ),
-                    children: [
-                      ...verses.map((verseNumber) {
-                        final verseText =
-                            quran.getVerse(surahNumber, verseNumber);
-                        final verseNumberArabic =
-                            convertToArabicNumerals(verseNumber.toString());
-                        return TextSpan(
-                          text: '$verseText $verseNumberArabic ',
-                        );
-                      }),
-                    ],
-                  ),
-                )
-              ],
-            ),
-          ),
-        );
-      },
-    );
-  }
-}
-
-class ChapterHeader extends StatelessWidget {
-  const ChapterHeader({
-    super.key,
-    required this.surahNumber,
-  });
-
-  final int surahNumber;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      height: 40,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-            image: AssetImage(
-              'assets/images/sharing_chapter_header.png',
-            ),
-            fit: BoxFit.fill),
-      ),
-      child: Center(
-        child: Text(
-          'سورة ${quran.getSurahNameArabic(surahNumber)}',
-          style: TextStyle(
-            fontFamily: 'UthmanicHafs',
-            fontSize: 24,
-            color: Theme.of(context).colorScheme.onSurface,
           ),
         ),
       ),
