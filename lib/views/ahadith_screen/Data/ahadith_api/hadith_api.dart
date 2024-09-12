b51f2008d71beccad495bf46d031134dc59f4c51
MohamedElsay0d <me827794@gmail.com>
2024-09-12 05:25:39 -0700

    create api,repo,model for hadith

---
+++ lib/views/ahadith_screen/Data/ahadith_api/hadith_api.dart
@@ -0,0 +1,29 @@
+import 'dart:developer';
+
+import 'package:dio/dio.dart';
+
+class HadithApi {
+  late Dio dio;
+  HadithApi() {
+    dio = Dio();
+  }
+
+  Future<List<dynamic>> getAhadith({required String type}) async {
+    try {
+      final response = await dio
+          .get('https://hadis-api-id.vercel.app/hadith/$type?page=1&limit=50');
+      if (response.data != null && response.data['items'] != null) {
+        return response.data['items'];
+      } else {
+        log('The expected key is missing in the response.');
+        return [];
+      }
+    } on DioException catch (e) {
+      log('DioException: ${e.toString()}');
+      return [];
+    } catch (e) {
+      log('An unexpected error occurred: ${e.toString()}');
+      return [];
+    }
+  }
+}
