44701202abcf0508b7716ed577a8e3325e511716
Muhamed-Hashesh <muhamed7as4h@gmail.com>
2024-09-17 23:28:30 +0300

    home nav

--- lib/views/custom_drawer/widgets/settings_tab.dart
+++ lib/views/custom_drawer/widgets/settings_tab.dart
@@ -14,61 +14,51 @@ class SettingsTab extends StatelessWidget {
       ),
       'label': 'القرآن الكريم',
       'hasSwitch': false,
-      'nav': SurahDetailScreen(surahNumber: 2)
     },
     {
       'icon': const Icon(Iconsax.icon),
       'label': 'الأحزاب',
       'hasSwitch': false,
-      'nav': SurahDetailScreen(surahNumber: 3)
     },
     {
       'icon': const Icon(Iconsax.icon),
       'label': 'التفسير',
       'hasSwitch': false,
-      'nav': SurahDetailScreen(surahNumber: 4)
     },
     {
       'icon': const Icon(Iconsax.icon),
       'label': 'الأجزاء',
       'hasSwitch': false,
-      'nav': SurahDetailScreen(surahNumber: 5)
     },
     {
       'icon': const Icon(Iconsax.book),
       'label': 'السجدات',
       'hasSwitch': false,
-      'nav': SurahDetailScreen(surahNumber: 6)
     },
     {
       'icon': const Icon(Iconsax.icon),
       'label': 'الركوع',
       'hasSwitch': false,
-      'nav': SurahDetailScreen(surahNumber: 7)
     },
     {
       'icon': const Icon(Iconsax.home),
       'label': 'الصفحة الرئيسية',
       'hasSwitch': false,
-      'nav': const EveryTypesView(), // Ensure it's a widget
     },
     {
       'icon': const Icon(Iconsax.moon),
       'label': 'تفعيل الوضع الليلي',
       'hasSwitch': true,
-      'nav': SizedBox(), // This will not navigate
     },
     {
       'icon': const Icon(Iconsax.emoji_happy),
       'label': 'المزيد من التطبيقات',
       'hasSwitch': false,
-      'nav': SizedBox(), // This will not navigate
     },
     {
       'icon': const Icon(Iconsax.share),
       'label': 'مشاركة التطبيق',
       'hasSwitch': false,
-      'nav': SizedBox(), // This will not navigate
     },
   ];
 
